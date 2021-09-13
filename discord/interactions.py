77df1b0054179cfd205a4e0e232ce719d8f2f969
nirlep5252 <hellonirlep@gmail.com>
2021-09-13 11:18:04 +0530

    returning message in send_message

--- discord/interactions.py
+++ discord/interactions.py
@@ -482,7 +482,7 @@ class InteractionResponse:
         view: View = MISSING,
         tts: bool = False,
         ephemeral: bool = False,
-    ) -> None:
+    ) -> Optional[Message]:
         """|coro|
 
         Responds to this interaction by sending a message.
@@ -561,6 +561,8 @@ class InteractionResponse:
             self._parent._state.store_view(view)
 
         self._responded = True
+
+        return self._parent.message
 
     async def edit_message(
         self,
